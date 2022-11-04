292fb0ba8d258561148b497f8b2091bff482c457
Ninja Tec <contato.esdrassantos@gmail.com>
2022-11-04 15:59:13 -0300

    Aula de métodos

---
+++ java-metodos/src/Metodos.java
@@ -0,0 +1,13 @@
+public class Metodos {
+    public static void main(String[] args) {
+        double n1 = 1.235;
+        double n2 = 23;
+
+        System.out.print(somar(n1,n2));
+        
+    }
+
+    public static double somar (double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+}
