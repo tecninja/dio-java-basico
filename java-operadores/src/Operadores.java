057bb2777b4be8cb5b533f660f56a3d8ee78dbc2
Ninja Tec <contato.esdrassantos@gmail.com>
2022-11-04 12:28:52 -0300

    Atualizações operadores comit 2

--- java-operadores/src/Operadores.java
+++ java-operadores/src/Operadores.java
@@ -13,5 +13,39 @@ public class Operadores {
         System.out.print(resultado);
 
         // O operador + quando usado em variáveis do tipo string tem função de concatenar as variáveis
+        
+        // ++ Operação de adição de uma unidade a variável
+        // -- Operação de subtração de uma unidade a variável
+
+        resultado++;
+
+        System.out.print(resultado);
+
+        // ! Operador de negação para valor booleano
+
+        boolean teste = true;
+
+        System.out.print(!teste);
+
+        // Operador Ternário (simula ifelse)
+
+        int a = 1;
+        int b = 1;
+
+        boolean resultado1 = a==b ?true : false;
+
+        System.out.print(resultado1);
+
+        // > Operador de maior que
+        // < Operador de menor que
+        // >= Operador de maior ou igual que
+        // <= Operador de menor ou igual que
+        // != Operador de diferente de
+
+        // Operadores Lógicos
+        // && Operador lógico 'E'
+        // || Operador lógico 'OU'
+        
+
     }
 }
