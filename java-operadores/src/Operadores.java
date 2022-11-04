4b832c6bf9ba8f3c0043d82449ee0ffddf5cd45b
Ninja Tec <contato.esdrassantos@gmail.com>
2022-11-04 10:51:58 -0300

    aula de operadores comit 1

---
+++ java-operadores/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // = Operador de Atribuição
+        // + Operador de Adição
+        // - Operador de subtração
+        // * Operador de multiplicação
+        // / operador de divisão
+        // % Operador de divisão inteira (retorna o resto da divisão)
+        int n1 = 2;
+        int n2 = 1;
+        int resultado = n1 + n2 * n2 - n1 / n1 % n2;
+
+        System.out.print(resultado);
+
+        // O operador + quando usado em variáveis do tipo string tem função de concatenar as variáveis
+    }
+}
