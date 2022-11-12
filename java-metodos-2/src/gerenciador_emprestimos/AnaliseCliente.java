c24eb6769fe25cdac91d0f4407216ee38fedcffc
Ninja Tec <contato.esdrassantos@gmail.com>
2022-11-12 11:11:33 -0300

    Criação da Classe AnaliseCliente

--- java-metodos-2/src/gerenciador_emprestimos/AnaliseCliente.java
+++ java-metodos-2/src/gerenciador_emprestimos/AnaliseCliente.java
@@ -1,5 +1,12 @@
 package gerenciador_emprestimos;
 
+/*
+ * Classe deve analisar capacidade de pagamento do cliente
+ */
 public class AnaliseCliente {
+
+    public static double verificarCapacidade(Cliente cliente) {
+        return 0.0;
+    }
     
 }
