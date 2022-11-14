336e965f0d71d7236d654c347d6309c52335f1f1
Ninja Tec <contato.esdrassantos@gmail.com>
2022-11-14 12:37:40 -0300

    Atualização do Javadoc

--- java-metodos-2/src/gerenciador_emprestimos/Cliente.java
+++ java-metodos-2/src/gerenciador_emprestimos/Cliente.java
@@ -20,24 +20,44 @@ public class Cliente {
     int tempoTrabalho; //Em anos
     boolean funcionarioPublico; //Receber false inplica automaticamente em ser funcionário da iniciativa privada
 
+    /**
+     * Método para popular o atributo Nome
+     * @param scan
+     * @return string Recebe o nome do cliente
+     */
     public static String atribuirNome(Scanner scan){
         System.out.println("Nome cliente: ");
         String nome = scan.next();
         return nome;
     }
 
+    /**
+     * Método para popular o atributo Data de Nascimento
+     * @param scan
+     * @return string recebe a data de nascimento do cliente
+     */
     public static String atribuirDataNascimento(Scanner scan){
         System.out.println("Data de nascimento: [DD-MM-YYYY]");
         String data = scan.next();
         return data;
     }
 
+    /**
+     * Método para popular o atributo Salário
+     * @param scan
+     * @return double recebe o valor do salário do cliente
+     */
     public static double atribuirSalario(Scanner scan){
         System.out.println("Salario: [Usar 'virgula' como separador decimal]");
         double salario = scan.nextDouble();
         return salario;
     }
 
+    /**
+     * Método para popular o atributo conta corrente
+     * @param scan
+     * @return boolean true para cliente com conta corrente, e false sem conta corrente
+     */
     public static boolean atribuirContaCorrente(Scanner scan){
         System.out.println("Possui conta conrrente? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -50,6 +70,11 @@ public class Cliente {
         return contaCorrente;
     }
 
+    /**
+     * Método para popular o atributo Cartão de Crédito
+     * @param scan
+     * @return boolean true para cliente com cartão de crédito, e false para cliente sem cartão de crédito
+     */
     public static boolean atribuirCartaoCredito(Scanner scan){
         System.out.println("Possui cartao de crédito? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -62,6 +87,11 @@ public class Cliente {
         return cartao;
     }
 
+    /**
+     * Método para popular o atributo Negativação
+     * @param scan
+     * @return boolean true para cliente negativado, e false para não negativado
+     */
     public static boolean atribuirNegativacao(Scanner scan){
         System.out.println("Possui negativacao? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -74,6 +104,12 @@ public class Cliente {
         return negativacao;
     }
 
+    /**
+     * Método para popular o atributo financiamento
+     * Deve indicar se o cliente possui financiamento com a instituição
+     * @param scan
+     * @return boolean true para cliente funcionário público, e false para iniciativa privada
+     */
     public static boolean atribuirFinanciamento(Scanner scan){
         System.out.println("Possui financiamento? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -86,6 +122,12 @@ public class Cliente {
         return financiamento;
     }
 
+    /**
+     * Método para popular o atributo investimento
+     * Deve indicar se o cliente possui valores investido ou não
+     * @param scan
+     * @return boolean true para cliente com investimento, e false para sem investimento
+     */
     public static boolean atribuirInvestimento(Scanner scan){
         System.out.println("Possui investimentos? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -98,6 +140,12 @@ public class Cliente {
         return investimento;
     }
 
+    /**
+     * Método para popular o atributo Autorização Gerencial
+     * Atributo permitirá que o cliente siga com a proposta de empréstimo, mesmo com score externo menor que 600
+     * @param scan
+     * @return boolean true para cliente com autorização gerencial, e false para cliente sem autorização.
+     */
     public static boolean atribuirAutorzacaoGerencial(Scanner scan){
         System.out.println("Possui autorização gerencial? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -110,6 +158,11 @@ public class Cliente {
         return autorizacao;
     }
 
+    /**
+     * Método para popular o atributo Funcionário Publico
+     * @param scan
+     * @return boolean true para cliente funcionário público, e false para iniciativa privada
+     */
     public static boolean atribuirFuncionarioPublico(Scanner scan){
         System.out.println("É funcionário público? [1: Sim 0: Não]");
         String resposta = scan.next();
@@ -122,26 +175,43 @@ public class Cliente {
         return funcionarioPublico;
     }
 
+    /**
+     * Método para popular o atributo Score Interno
+     * @param scan
+     * @return int score em pontos 0 a 100
+     */
     public static int atribuirScoreInterno(Scanner scan){
         System.out.println("Informe o score interno:");
         int score = scan.nextInt();
         return score;
     }
 
+    /**
+     * Método para popular o atributo Score Externo
+     * @param scan
+     * @return int score em pontos 0 a 1000
+     */
     public static int atribuirScoreExterno(Scanner scan){
         System.out.println("Informe o score externo:");
         int score = scan.nextInt();
         return score;
     }
-
+    /**
+     * Método para popular o atributo Tempo de Trabalho
+     * @param scan
+     * @return int tempo de trabalho em anos
+     */
     public static int atribuirTempoTrbalho(Scanner scan){
         System.out.println("Informe o tempo de trabalho atual:");
         int tempo = scan.nextInt();
         return tempo;
     }
 
-    /*
-     * @params
+    /**
+     * Método para popular os atributos
+     * 
+     * @param cliente recebe um objeto do mesmo tipo da classe que está inserida
+     * @return retorna o objeto Cliente com todos os atributos preenchidos
      */
     public static Cliente cadastrarCliente(Cliente cliente) {
 
